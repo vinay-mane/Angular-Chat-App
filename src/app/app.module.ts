2115b02935450dd9348d6a5b925365db41aba549
vinay-mane <manevinay44+github@gmail.com>
2023-09-01 14:09:27 +0530

    ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './comp/side-bar/side-bar.component';
+import { ChatComponent } from './comp/chat/chat.component';
+import { NavComponent } from './comp/nav/nav.component';
+import { SendMessageComponent } from './comp/send-message/send-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    ChatComponent,
+    NavComponent,
+    SendMessageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/comp/side-bar/side-bar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.scss']
+})
+export class SideBarComponent {
+  users = [
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+    {'name':'vinay'},
+  ]
+}
